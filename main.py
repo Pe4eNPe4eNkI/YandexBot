8256082a34354fea6d811ae852e4e1541bc8ad7c
BelkovskiDaniil <belka.danya1939@mail.ru>
2021-03-31 22:05:20 +0500

    Начальные пробы в бота

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-print("BillyHerrington")
-print('dddd')
-print("BillyHerrington")
-print('dddd')
-print("BillyHerrington")
+import discord
+from discord.ext import commands
+from discord.utils import get
+
+client = commands.Bot(command_prefix='.')
+client.remove_command('help')
+
+hello_world = ['привет', 'hi', 'Hi', 'Hello', 'hello', 'qq', 'q', 'ky', 'Привет', 'здравствуйте',
+               'Здравствуйте', 'Ку', 'здорова', 'Хеллоу', "хеллоу"]
+antword = ['информация', 'команды', 'help',
+           'Help', 'info', 'Info', 'что делать']
+
+# get token
+token = open('token.txt', 'r').readline()
+
+client.run(token)
