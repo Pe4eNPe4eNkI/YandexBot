ac5cb465f6276583f10d747f01eb2ed3203f218a
BelkovskiDaniil <belka.danya1939@mail.ru>
2021-04-01 21:21:42 +0500

    Бан.

--- main.py
+++ main.py
@@ -34,6 +34,23 @@ async def on_member_join(member):
         embed=discord.Embed(discription=f'Пользователь ''{member.name}'' присоединился к серверу!',
                             color=0x0c0c0c))
 
+# ban
+@client.command(pass_context=True)
+@commands.has_permissions(administrator=True)
+async def ban(ctx, member: discord.Member, *, reason=None):
+    emb = discord.Embed(title='Ban', color=discord.Color.red())
+
+    await ctx.channel.purge(limit=1)
+    await member.ban(reason=reason)
+
+    emb.set_author(name=member.name)
+    emb.add_field(name='Ban user',
+                  value='Banned user: {}'.format(member.mention))
+    emb.set_footer(
+        text='Был забанен администратором{}'.format(ctx.author.name))
+
+    await ctx.send(embed=emb)
+
 # get token
 token = open('token.txt', 'r').readline()
 
